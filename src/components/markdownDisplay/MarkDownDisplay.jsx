a248d175e1215a25464f3b08b944a9cb9b05eecb
Joseph Ehigiator <joeehis1@gmail.com>
2023-12-11 13:47:59 -0500

    Added support for single line breaks

--- src/components/markdownDisplay/MarkDownDisplay.jsx
+++ src/components/markdownDisplay/MarkDownDisplay.jsx
@@ -25,6 +25,7 @@ export default function MarkDownDisplay({ text }) {
         const converter = new Converter({
             extensions: [...bindings],
             tables: true,
+            simpleLineBreaks: true,
         });
 
         converter.setFlavor("github");
